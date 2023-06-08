7eef4f42bc0935fc7f9464e2090cc29f24fb68cd
simplysabir <simplysabir@gmail.com>
2023-06-08 20:05:35 +0530

    ✨ feat : Books Model Added

---
+++ models/books.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const booksSchema = mongoose.Schema({
+    book_id : {
+        type : Number,
+        required : true,
+    },
+    book_name : {
+        type : String,
+        required : true,
+    },
+    book_rating : {
+        type : String,
+        required : true,
+    }
+
+});
+
+const Book = mongoose.model("Books", booksSchema);
+
+module.exports = Book;
